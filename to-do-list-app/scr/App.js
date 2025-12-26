cc1ac9148138099e3821cdee5da6eb260ee4c85c
Thulio Nicodemos <160731962+thulionicodemos@users.noreply.github.com>
2025-12-25 23:49:32 -0300

    Initialize App component with routing

---
+++ to-do-list-app/scr/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as Icon from 'react-bootstrap-icons';
+import React, {useState, useEffect, useNemo, useCallback} from 'react';
+
+import RoutesApp from './routes/routes';
+
+function App() {
+  return (
+    <RoutesApp />
+  );
+}
+
+export default App;
