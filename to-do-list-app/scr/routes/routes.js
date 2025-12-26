23a7c3154ddd4f00c423b0b66d74b7c5569b9bd3
Thulio Nicodemos <160731962+thulionicodemos@users.noreply.github.com>
2025-12-25 23:50:43 -0300

    Add files via upload

---
+++ to-do-list-app/scr/routes/routes.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/home";
+import Register from "../pages/register";
+import Admin from "../pages/admin";
+import Private from "./private";
+
+function RoutesApp() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/register" element={<Register />} />
+				<Route
+					path="/admin"
+					element={
+						<Private>
+							<Admin />
+						</Private>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default RoutesApp;
